4552dc6c14f6ee1c67cf58231797d1b82f4b43d3
Josep Anguera Peralta <josep.anguera@wallapop.com>
2022-12-05 00:10:06 +0100

    2022 - Day 1

---
+++ day1/problem1.py
@@ -0,0 +1,18 @@
+def main(fn):
+    elves = []
+    with open(fn) as f:
+        lines = f.readlines()
+    current_elf = 0
+    for line in lines:
+        line = line.strip()
+        if not line:
+            elves.append(current_elf)
+            current_elf = 0
+            continue
+        current_elf += int(line)
+    elves.append(current_elf)
+    print(max(elves))
+
+
+if __name__ == "__main__":
+    main("input.txt")
